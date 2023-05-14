29b020871e0bfecd2a019b6291fb26a42b99141a
michaelangelesz <mmweisphal@outlook.com>
2023-05-14 19:06:10 -0400

    add arrow key functionality to update direction variable

--- move.js
+++ move.js
@@ -12,6 +12,19 @@ function move(element) {
 }
 
 //add event listener for keydown (user presses an arrow key)
-document.addEventListener('keydown', function(e){
-
+document.addEventListener('keydown', function(e){                
+    //add functionality to update direction variable based on the arrow key pressed. if user presses the left arrow, our character moves west, and so on
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
 })
+
