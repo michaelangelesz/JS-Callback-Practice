1c114ae28a6fcfc82033bb99c223b9482fecf07b
michaelangelesz <mmweisphal@outlook.com>
2023-05-14 18:16:27 -0400

    Updated character position based on direction using conditional statements and updated element attributes

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ let y = 250; //create y variable to track vertical position
 move(character).to(100, 250)
 
 
+    //this code checks if the direction variable is west and moves our character one pixel to the left on the screen if it is
+    if(direction === 'west'){
+        x = x - 1
+    }
+    //this code checks if the direction variable is north and moves our character one pixel up on the screen if it is
+    if(direction === 'north'){
+        y = y + 1
+    }
+    //this code checks if the direction variable is east and moves our character one pixel to the right on the screen if it is
+    if(direction === 'east'){
+        x = x + 1
+    }
+    //this code checks if the direction variable is south and moves our character one pixel down on the screen if it is
+    if(direction === 'south'){
+        y = y - 1
+    }
+    //this code updates the position of our character on the screen using the x and y variables
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+
+
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
