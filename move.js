c463d71461a243aac18a09347723ce88061dd1b4
michaelangelesz <mmweisphal@outlook.com>
2023-05-14 18:52:08 -0400

    add event listener for keydown when user presses an arrow key

--- move.js
+++ move.js
@@ -10,3 +10,8 @@ function move(element) {
         to: moveToCoordinates
     }
 }
+
+//add event listener for keydown (user presses an arrow key)
+document.addEventListener('keydown', function(e){
+
+})
